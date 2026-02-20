963378e5fbe53c0bdd53bd549fb4e34d9a670a25
AltynaiGit <altynai.777.777.777@gmail.com>
2026-02-20 19:34:06 +0600

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     static void main() {
+        Scanner scn = new Scanner(System.in);
 
+        List<String> list = new ArrayList<>();
+        list.add("Daniel");
+        list.add("Altynai");
+        list.add("Islan");
+        list.add("Sanzhar");
+        list.add("Askat");
+        list.add("Kutman");
+
+        String user = scn.nextLine();
+
+        if (list.contains(user)) {
+            System.out.println("Правильно");
+        } else {
+            System.out.println("Неправильно ввели имя");
+        }
     }
 }

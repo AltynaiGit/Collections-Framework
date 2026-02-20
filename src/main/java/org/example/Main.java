2e88289e856e49dbbb8a4bd3a58879a73ea7b80c
AltynaiGit <altynai.777.777.777@gmail.com>
2026-02-20 18:32:03 +0600

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        List<Integer> arrays = new ArrayList<>();
+        arrays.add(1);
+        arrays.add(2);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        System.out.println("array: " + arrays.get(0));
+
+        List<Integer> arraysLInked = new LinkedList<>();
+        arraysLInked.get(11);
+        arraysLInked.get(22);
+
+        System.out.println("Linked: " + arraysLInked.get(1));
+
     }
 }

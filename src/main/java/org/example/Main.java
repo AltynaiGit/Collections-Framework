1b78778efcc6a31d0550ca9718a067ea38f7b477
AltynaiGit <altynai.777.777.777@gmail.com>
2026-02-20 19:52:30 +0600

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     static void main() {
+        List<Integer> arrays = new ArrayList<>();
+        arrays.add(5);
+        arrays.add(15);
+        arrays.add(30);
+        arrays.add(50);
+
+        int sum = 0;
+        for (Integer numbers: arrays){
+            int a = numbers;
+            sum +=a;
+        }
+
+        System.out.println("Итого: " + sum);
 
     }
 }
